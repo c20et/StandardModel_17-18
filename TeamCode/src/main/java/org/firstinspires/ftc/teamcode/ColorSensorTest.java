f5c5bce5c257cf34486a2abab7225f6f760a9223
c20am <31973507+c20am@users.noreply.github.com>
2017-10-26 18:46:23 -0400

    edit color sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
@@ -4,33 +4,37 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.view.View;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 
-import org.firstinspires.ftc.teamcode.Robot;
-
-@Autonomous(name = "Color Sensor Color", group = "Sensor")
+@Autonomous(name = "Color Sensor", group = "Sensor")
+@Disabled
 public class ColorSensorTest extends LinearOpMode {
 
     ColorSensor colorSensorR;
     ColorSensor colorSensorL;
+    // Hardware Device Object
 
 
     @Override
     public void runOpMode() {
+
         colorSensorR = hardwareMap.get(ColorSensor.class, "color sensor right");
         colorSensorL = hardwareMap.get(ColorSensor.class, "color sensor left");
 
-        while(opModeIsActive()) {
-            telemetry.addData("RGB VALS (R)", "red (%.2f), blue (%.2f), green (%.2f)", colorSensorR.red() , colorSensorR.blue(), colorSensorR.green());
+        // wait for the start button to be pressed.
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            // send the info back to driver station using telemetry function.
+            telemetry.addData("RGB VALS (R): ", "red (%.2f), blue (%.2f), green (%.2f)", colorSensorR.red() , colorSensorR.blue(), colorSensorR.green());
+            telemetry.addData("RGB VALS (L): ", "red (%.2f), blue (%.2f), green (%.2f)", colorSensorL.red() , colorSensorL.blue(), colorSensorL.green());
+
+            telemetry.update();
         }
 
-
     }
-
-
 }
-
